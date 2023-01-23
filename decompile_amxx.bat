87373aeeadffd4732ce7b8ed051d4067ee4e61ab
Karaulov <unrealkaraulov@bk.ru>
2023-01-23 13:38:22 +0300

    REupload

--- decompile_amxx.bat
+++ decompile_amxx.bat
@@ -1,1 +1,1 @@
-"c:\Program Files\Java\jre1.8.0_341\bin\java.exe" -jar "c:\Projects\AMX_DECOMPILER\lysis-java\build\libs\lysis-java.jar" %1 %1.sma
+"c:\Program Files\Java\jdk-19\bin\java.exe" -jar "c:\Projects\AMX_DECOMPILER\lysis-java\build\libs\lysis-java.jar" %1 %1.sma
